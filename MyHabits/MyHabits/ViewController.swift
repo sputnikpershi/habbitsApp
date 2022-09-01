9499fde5e41f541a921698e31b670132905b4ca3
Kiryl <sputnik.pershi@ya.ru>
2022-09-01 12:55:57 +0800

    Initial commint

---
+++ MyHabits/MyHabits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyHabits
+//
+//  Created by Krime Loma    on 9/1/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .lightGray
+        
+    }
+
+
+}
+
